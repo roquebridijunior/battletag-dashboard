d7171380acd57e08866ab0096e8de3777f2deb71
Rock <39769655+roquebridijunior@users.noreply.github.com>
2020-05-18 21:15:01 -0300

    novo menu

--- src/pages/pacotes.js
+++ src/pages/pacotes.js
@@ -1,81 +1,81 @@
 import React from "react";
-import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import TemplateMenu from "../templates/template-menu.js";
+//import TemplateMenu from "../templates/template-menu.js";
+import TemplateMenu2 from "../templates/template-menu2.js";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
   paper: {
     padding: theme.spacing(2)
+  },
+  grid: {
+    padding:  theme.spacing(2)
   }
 }));
 
 export default function Painel() {
   const classes = useStyles();
-  const fixedHeightPaper = clsx(classes.paper);
 
   return (
-    <div>
-      <TemplateMenu>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6} lg={4}>
-            <Paper className={fixedHeightPaper}>
-              <Typography gutterBottom variant="h5" component="h2">
-                Pacote Soldado
-              </Typography>
-              <Typography component="p">
-                100 BatteTag Ammo por R$ 1,00
-              </Typography>
-              <Button variant="contained" component={Link} to="/compra/1">
-                Comprar
-              </Button>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Paper className={fixedHeightPaper}>
-              <Typography gutterBottom variant="h5" component="h2">
-                Pacote Sargento
-              </Typography>
-              <Typography component="p">
-                1.000 BatteTag Ammo por R$ 9,90
-              </Typography>
-              <Button variant="contained" component={Link} to="/compra/2">
-                Comprar
-              </Button>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Paper className={fixedHeightPaper}>
-              <Typography gutterBottom variant="h5" component="h2">
-                Pacote Tenente
-              </Typography>
-              <Typography component="p">
-                10.000 BatteTag Ammo por R$ 95,00
-              </Typography>
-              <Button variant="contained" component={Link} to="/compra/3">
-                Comprar
-              </Button>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Paper className={fixedHeightPaper}>
-              <Typography gutterBottom variant="h5" component="h2">
-                Pacote Capitão
-              </Typography>
-              <Typography component="p">
-                100.000 BatteTag Ammo por R$ 900,00
-              </Typography>
-              <Button variant="contained" component={Link} to="/compra/4">
-                Comprar
-              </Button>
-            </Paper>
-          </Grid>
+    <TemplateMenu2>
+      <Grid container spacing={2} className={classes.grid}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Paper className={classes.paper}>
+            <Typography gutterBottom variant="h5" component="h2">
+              Pacote Soldado
+            </Typography>
+            <Typography component="p">
+              100 BatteTag Ammo por R$ 1,00
+            </Typography>
+            <Button variant="contained" component={Link} to="/compra/1">
+              Comprar
+            </Button>
+          </Paper>
         </Grid>
-      </TemplateMenu>
-    </div>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Paper className={classes.paper}>
+            <Typography gutterBottom variant="h5" component="h2">
+              Pacote Sargento
+            </Typography>
+            <Typography component="p">
+              1.000 BatteTag Ammo por R$ 9,90
+            </Typography>
+            <Button variant="contained" component={Link} to="/compra/2">
+              Comprar
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Paper className={classes.paper}>
+            <Typography gutterBottom variant="h5" component="h2">
+              Pacote Tenente
+            </Typography>
+            <Typography component="p">
+            10.000 BatteTag Ammo por R$ 95,00
+            </Typography>
+            <Button variant="contained" component={Link} to="/compra/3">
+              Comprar
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Paper className={classes.paper}>
+            <Typography gutterBottom variant="h5" component="h2">
+              Pacote Capitão
+            </Typography>
+            <Typography component="p">
+              100.000 BatteTag Ammo por R$ 900,00
+            </Typography>
+            <Button variant="contained" component={Link} to="/compra/4">
+              Comprar
+            </Button>
+          </Paper>
+        </Grid>
+      </Grid>
+    </TemplateMenu2>
   );
 }
---
+++ src/templates/template-menu2.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import clsx from 'clsx';
+import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListSubheader from "@material-ui/core/ListSubheader";
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from "@material-ui/core/Divider";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Badge from "@material-ui/core/Badge";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+
+import AccountCircletIcon from "@material-ui/icons/AccountCircle";
+import ReceiptIcon from "@material-ui/icons/Receipt";
+import StoreIcon from "@material-ui/icons/Store";
+import MenuIcon from "@material-ui/icons/Menu";
+import SwapHorizIcon from "@material-ui/icons/SwapHoriz";
+import SwapHorizontalCircleIcon from "@material-ui/icons/SwapHorizontalCircle";
+import ReceiptOutlinedIcon from "@material-ui/icons/ReceiptOutlined";
+import BarChartIcon from "@material-ui/icons/BarChart";
+
+export default function TemplateMenu2(props) {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const [state, setState] = React.useState();
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState(open);
+  };
+
+  return (
+    <div>
+      <React.Fragment>
+        <Drawer open={state} onClose={toggleDrawer(false)} onOpen={toggleDrawer(true)}>
+          <div
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+          >
+            <Typography variant="h5" component="h2">
+              BATTLETAG
+            </Typography>
+            <Divider />
+            <List component="nav">
+              <ListSubheader component="div">Loja</ListSubheader>
+              <ListItem button component={Link} to="/pacotes">
+                <ListItemIcon>
+                  <StoreIcon />
+                </ListItemIcon>
+                <ListItemText primary="Pacotes" />
+              </ListItem>
+              <ListSubheader component="div">Suporte</ListSubheader>
+              <ListItem button component={Link} to="/meus-chamados">
+                <ListItemIcon>
+                  <ReceiptOutlinedIcon />
+                </ListItemIcon>
+                <ListItemText primary="Meus Chamados" />
+              </ListItem>
+              <ListSubheader component="div">Conta</ListSubheader>
+              <ListItem button component={Link} to="/minhas-transacoes">
+                <ListItemIcon>
+                  <SwapHorizIcon />
+                </ListItemIcon>
+                <ListItemText primary="Minhas Transações" />
+              </ListItem>
+              <Divider />
+              <ListSubheader component="div">Admin</ListSubheader>
+              <ListItem button component={Link} to="/saldos">
+                <ListItemIcon>
+                  <BarChartIcon />
+                </ListItemIcon>
+                <ListItemText primary="Saldos" />
+              </ListItem>
+              <ListItem button component={Link} to="/transacoes">
+                <ListItemIcon>
+                  <SwapHorizontalCircleIcon />
+                </ListItemIcon>
+                <ListItemText primary="Transações" />
+              </ListItem>
+              <ListItem button component={Link} to="/chamados">
+                <ListItemIcon>
+                  <ReceiptIcon />
+                </ListItemIcon>
+                <ListItemText primary="Chamados" />
+              </ListItem>
+            </List>
+          </div>
+        </Drawer>
+      </React.Fragment>
+      <AppBar>
+        <Toolbar >
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+          >
+            Painel 
+          </Typography>
+          <IconButton color="inherit">
+            <Badge>
+              <AccountCircletIcon
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              />
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose} component={Link} to="/login">
+                  Login
+                </MenuItem>
+                <MenuItem onClick={handleClose} component={Link} to="/logout">
+                  Logout
+                </MenuItem>
+              </Menu>
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      {props.children}
+    </div>
+  );
+}
